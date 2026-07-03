a5af558b9c1616b81ec6fddb8729617d2b71d7c0
hwidifiras <hwidifiras@gmail.com>
2026-07-03 03:46:19 +0200

    Fix account menu effect import

--- src/components/layout/user-account-menu.tsx
+++ src/components/layout/user-account-menu.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useRef, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import Link from "next/link";
 import { useRouter } from "next/navigation";
 import { Activity, ChevronDown, CircleUser, LogOut, Users } from "lucide-react";
